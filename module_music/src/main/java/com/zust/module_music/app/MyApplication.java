47a58a18ec171ba6fc1c79d788c0f8feb66445d4
532268948 <5322268948@qq.com>
2018-12-08 17:33:07 +0800

    创建铃声组件和沉浸式状态栏工具类

---
+++ module_music/src/main/java/com/zust/module_music/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.zust.module_music.app;
+
+import com.example.lib_common.base.BaseApplication;
+
+/**
+ * project: Monkey
+ * author : 叶天华
+ * date   : 2018/11/25
+ * time   : 14:19
+ * email  : 15869107730@163.com
+ * note   :
+ */
+public class MyApplication extends BaseApplication {
+}
---
+++ module_music/src/main/java/com/zust/module_music/container/ContainerActivity.java
@@ -0,0 +1,15 @@
+package com.zust.module_music.container;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zust.module_music.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
